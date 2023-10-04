3f7779cb039f3be3aadd7e5201bfddd9078777ab
CatYulHa <checkdata.yul@gmail.com>
2023-10-04 14:35:31 +0900

    2023-10-04 임시수정

--- FA/main.py
+++ FA/main.py
@@ -1,3 +1,4 @@
+### 필요 라이브러리 등
 from fastapi import FastAPI, Depends, HTTPException, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
@@ -8,23 +9,44 @@ from databases import Database
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.staticfiles import StaticFiles
 
+### 서버 실행 코드: uvicorn main:app --reload ###
+
+# 데이터베이스 연결 URL 설정
 DATABASE_URL = "mysql+pymysql://root:0000@127.0.0.1:3306/security"
 
+# 데이터베이스 객체 생성
 database = Database(DATABASE_URL)
 metadata = MetaData()
 
+# FastAPI 앱 객체 생성
 app = FastAPI()
 
-# 'static' 디렉토리에 있는 정적 파일을 '/static' 경로로 제공합니다.
+# 'static' 디렉토리의 정적 파일을 '/static' 경로로 제공
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# 세션 미들웨어 추가
+# 'bootstrap' 디렉토리의 정적 파일을 '/static' 경로로 제공
+app.mount("/bootstrap", StaticFiles(directory="bootstrap"), name="bootstrap")
+
+# 'css' 디렉토리의 정적 파일을 '/static' 경로로 제공
+app.mount("/css", StaticFiles(directory="css"), name="css")
+
+# 'js' 디렉토리의 정적 파일을 '/static' 경로로 제공
+app.mount("/js", StaticFiles(directory="js"), name="js")
+
+
+
+
+#########################################################
+# 세션 미들웨어 추가 (사용자 세션 관리를 위함)
 app.add_middleware(SessionMiddleware, secret_key="your-secret-key")
 
+# Jinja2 템플릿 설정
 templates = Jinja2Templates(directory="templates")
 
+# SQLAlchemy의 기본 클래스 생성
 Base = declarative_base()
 
+# User 모델 정의
 class User(Base):
     __tablename__ = "security"
     sec_id = Column(String, primary_key=True, index=True)
@@ -35,6 +57,7 @@ class User(Base):
     sec_hp = Column(String)
     sec_area = Column(String)
 
+# 루트 경로에 대한 핸들러
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     user_id = request.session.get("user_id")
@@ -44,10 +67,17 @@ async def read_root(request: Request):
         user = await database.fetch_one(query)
     return templates.TemplateResponse("index.html", {"request": request, "user": user})
 
+# 로그인 페이지에 대한 핸들러
 @app.get("/login", response_class=HTMLResponse)
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
+# 로그인 페이지에 대한 핸들러
+@app.get("/signup", response_class=HTMLResponse)
+async def signup_page(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
+
+# 로그인 처리 핸들러
 @app.post("/login/")
 async def login(request: Request, sec_id: str = Form(...), sec_pw: str = Form(...)):
     query = select(User).where(User.sec_id == sec_id)
@@ -60,16 +90,53 @@ async def login(request: Request, sec_id: str = Form(...), sec_pw: str = Form(..
     request.session["user_id"] = sec_id
     return RedirectResponse(url="/", status_code=303)
 
+# 로그아웃 처리 핸들러
 @app.get("/logout/")
 def logout(request: Request):
     if "user_id" in request.session:
         del request.session["user_id"]
     return RedirectResponse(url="/", status_code=303)
 
+# 앱 시작 시 데이터베이스 연결
 @app.on_event("startup")
 async def startup():
     await database.connect()
 
+# 앱 종료 시 데이터베이스 연결 해제
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
+
+### 회원 가입 기능 추가
+@app.post("/signup")
+async def signup(
+    request: Request,
+    sec_id: str = Form(...),
+    sec_pw: str = Form(...),
+    sec_nm: str = Form(...),
+    sec_bir: str = Form(...),
+    sec_hp: str = Form(...),
+    sec_add: str = Form(...),
+    sec_area: str = Form(...)
+):
+    # 이미 존재하는 사용자인지 확인
+    query = select(User).where(User.sec_id == sec_id)
+    user = await database.fetch_one(query)
+    if user:
+        raise HTTPException(status_code=400, detail="User ID already exists")
+
+    # 사용자 정보 저장
+    query = User.__table__.insert().values(
+        sec_id=sec_id,
+        sec_pw=sec_pw,
+        sec_nm=sec_nm,
+        sec_bir=sec_bir,
+        sec_hp=sec_hp,
+        sec_add=sec_add,
+        sec_area=sec_area
+    )
+    await database.execute(query)
+
+    # 로그인 페이지로 리다이렉트
+    return RedirectResponse(url="/login", status_code=303)
